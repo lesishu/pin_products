e4afb1230bc4d770f12fc4990d8fd871b8e14f31
Lesishu <lesishu@qq.com>
2018-01-05 20:58:58 +0800

    CSV导入到SQLite数据库

--- import.py
+++ import.py
@@ -8,10 +8,9 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+# 创建SQLite数据库连接
 conn = sqlite3.connect('products.sqlite')
 cursor = conn.cursor()
-
-exit(0)
 
 # 遍历CSV目录，获取CSV文件，创建文件列表
 csvfiles = []
@@ -21,14 +20,28 @@ for csvfile in os.listdir('products_csv/'):
 	else:
 		csvfiles.append(csvfile)
 
-for csvfile in csvfiles:
-	print csvfile
-	with open('products_csv/' + csvfile,'r') as csvtext:
-		lines = csv.reader(csvtext)
-		for line in lines:
-			url = line[0]
-			image = line[1]
-			text = line[2]
-			print url
-			print image
-			print text
+# 逐一导入CSV文件内容到SQLite数据库
+if csvfiles:
+	for csvfile in csvfiles:
+		print 'Importing', csvfile
+		with open('products_csv/' + csvfile,'r') as csvtext:
+			lines = csv.reader(csvtext)
+			i = 0
+			for line in lines:
+				i += 1
+				url = line[0]
+				image = line[1]
+				note = line[2]
+				sql = 'INSERT INTO products(url,image,note) VALUES("%s","%s","%s");' % (url,image,note)
+				try:
+					cursor.execute(sql)
+					conn.commit()
+				except:
+					conn.rollback()
+		# 导入完成删除CSV文件
+		os.remove('products_csv/' + csvfile)
+		print ' |- Importe', i, 'products'
+
+# 关闭SQLite数据库连接
+cursor.close()
+conn.close()
